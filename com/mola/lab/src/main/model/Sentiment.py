c9e7ac390bc6f4a5571c3cd3f65021d7c53297c5
SKsaqlain <sksaqlain25@gmail.com>
2023-01-08 23:33:47 +0530

    added sentiment-score-api-and-model

--- com/mola/lab/src/main/model/Sentiment.py
+++ com/mola/lab/src/main/model/Sentiment.py
@@ -2,18 +2,18 @@ from marshmallow import Schema, fields
 
 
 class Sentiment(object):
-    def __init__(self,tweet_text,sentiment_score,detected_mode):
+    def __init__(self,tweet_text,sentiment_score,detected_mood):
         self.tweet_text=tweet_text
         self.sentiment_score=sentiment_score
-        self.detected_mode=detected_mode
+        self.detected_mood=detected_mood
 
 class SentimentScoreSchema(Schema):
     positive=fields.Float()
     negative=fields.Float()
-    neutra=fields.Float()
+    neutral=fields.Float()
 
 class SentimentSchema(Schema):
     tweet_text=fields.Str()
     sentiment_score=fields.Nested(SentimentScoreSchema)
-    detected_mode=fields.Str()
+    detected_mood=fields.Str()
 
