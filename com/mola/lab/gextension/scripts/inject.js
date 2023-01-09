99a0b3ac0e9a23265f399eca707d8a558ff171d4
SKsaqlain <sksaqlain25@gmail.com>
2023-01-09 23:39:18 +0530

    added emoji-sentiment-logic-for-single-tweet

--- com/mola/lab/gextension/scripts/inject.js
+++ com/mola/lab/gextension/scripts/inject.js
@@ -1,41 +1,93 @@
 
 let xhr = new XMLHttpRequest()
-const url="http://localhost:5000/"
+const url=" http://127.0.0.1:5000"
+const positive=0x1F60A
+const sad=0x1F641
+const neutral=0x1F610
 
 let postObj={
     tweet_text:""
 }
 
+var requestBody=[]
+
 
 // data-testid="tweetText"
 
-function getTweet(){
-
-var a=document.querySelector('[data-testId=tweetText]');
-console.log(a)
-tweetNode=a.getElementsByTagName("span")[0]
-var tweetText=tweetNode.innerText
-console.log(tweetText)
-isEnglish(tweetText)
+function addSentimentEmoji(sentimentScore,tweet){
+    var time=tweet.getElementsByTagName("time")[0]
+    var p=document.createElement("p")
+    p.innerHTML="Detected Mood:"
+    if(sentimentScore[0].detected_mood==="NEUTRAL"){
+        p.innerHTML=p.innerHTML+String.fromCodePoint(neutral)
+    }
+    else if(sentimentScore[0].detected_mood==="POSITIVE")
+    {
+        p.innerHTML=p.innerHTML+String.fromCodePoint(positive)
+    }
+    else{
+        p.innerHTML=p.innerHTML+String.fromCodePoint(sad)
+    }
+    // time.parentNode.appendChild(p)
+    time.innerHTML=time.innerHTML+" "+p.innerHTML
 
 }
 
-
-function isEnglish(tweetText){
-    postObj['tweet_text']=tweetText
-    let post = JSON.stringify(postObj)
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+function getSentiment(tweetText,tweet){
+    postObj.tweet_text=tweetText
+    requestBody.push(postObj)
+    let post = JSON.stringify(requestBody)
+    console.log(post)
+    sentimentApi=url+"/"+"api/sentiment-score"
+    xhr.open('POST', sentimentApi, true)
+    xhr.setRequestHeader('Content-type', 'application/json')
+    xhr.setRequestHeader('Access-Control-Allow-Origin','*')
     xhr.send(post);
     xhr.onload = function () {
     if(xhr.status === 200) {
         console.log("Post successfully created!") 
-        console.log("response message"+xhr.response.message)
+        console.log("response message"+xhr.response)
+        response=JSON.parse(xhr.response);
+        console.log(response)
+        addSentimentEmoji(response,tweet)
     }
 }
 }
-setTimeout(load,5000)
+
+function isEnglish(tweetText,nodeContext){
+    postObj.tweet_text=tweetText
+    requestBody.push(postObj)
+    let post = JSON.stringify(requestBody)
+    console.log(post)
+    isEnglishApi=url+"/"+"/api/language-detection"
+    xhr.open('POST', isEnglishApi, true)
+    xhr.setRequestHeader('Content-type', 'application/json')
+    xhr.setRequestHeader('Access-Control-Allow-Origin','*')
+    xhr.send(post);
+    xhr.onload = function () {
+    if(xhr.status === 200) {
+        console.log("Post successfully created!") 
+        console.log("response message"+xhr.response)
+        response=JSON.parse(xhr.response);
+        console.log(response[0].is_english)
+        if(response[0].is_english){
+            getSentiment(tweetText,nodeContext)
+        }
+    }
+}
+}
+
+function getTweet(){
+// var a=document.querySelectorAll('[data-testId=tweetText]');
+var tweets= document.querySelectorAll('[data-testid=tweet]');
+
+//picking only timeline tweets
+tweetText=tweets[1].querySelector('[data-testId=tweetText]').getElementsByTagName("span")[0]
+var tweetText=tweetText.innerText
+console.log(tweetText)
+isEnglish(tweetText,tweets[1])
+
+}
 
 
-
-
+setTimeout(getTweet,5000)
